6d249c9038652ef89d9726ea2c379212492deb07
madhav <madhav201singh@gmail.com>
2024-03-26 12:14:02 +0530

    06-not-found-Done

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return(
+        <div>
+            <h2>Page Not Found</h2>
+            <p>Could not find reviewId more than 1000</p>
+        </div>
+    );
+}
