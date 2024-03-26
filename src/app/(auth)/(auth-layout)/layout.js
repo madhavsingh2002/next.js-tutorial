fee9f98f55a8d5dacd2b808b64687f49d1495b08
madhav <madhav201singh@gmail.com>
2024-03-26 20:10:53 +0530

    15. active-links

--- src/app/(auth)/(auth-layout)/layout.js
+++ src/app/(auth)/(auth-layout)/layout.js
@@ -1,7 +1,27 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import './style.css';
+const navLinks=[
+  {name:"Register",href:"/register"},
+  {name:"Login",href:"/login"},
+  {name:"Forget Password",href:"/forget-password"},
+]
 export default function AuthLayout({ children }) {
+  const pathname = usePathname();
+ 
     return (
         <>
-        <h2>Inner Layout</h2>
+        {navLinks.map((link)=>{
+           const isActive = pathname.startsWith(link.href)
+          return (
+            <Link href={link.href} key={link.name}
+            className={isActive?"font-bold mr-4":"text-red-500 mr-4"}
+            >
+              {link.name}
+            </Link>
+          )
+        })}
           {children}
           </>
     );
