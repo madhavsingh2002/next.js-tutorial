075cac2b1e90b97eb7dbf23bcba5e0cbc1233770
madhav <madhav201singh@gmail.com>
2024-03-26 18:33:38 +0530

    14-Link-Component-navigation

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export const metadata = {
     title:{
         absolute: "Blog"
     }
   };
 export default function Blog(){
-    return <h1>My Blog</h1>
+    return(
+        <>
+     <h1>My Blog</h1>
+     <Link href="/">Home</Link>
+     </>
+    )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+import Link from "next/link";
 export default function Home() {
   return (
+  <>  
+    <Link href="/blog">Blog</Link> <br/>
+    <Link href="/products">Products </Link>
     <h1>Home Page</h1>
+    </>
   );
 }
