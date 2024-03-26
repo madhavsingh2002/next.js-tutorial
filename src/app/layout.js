6d02009bd818ef86e8b0dee2ba8062d36673f9c4
madhav <madhav201singh@gmail.com>
2024-03-26 14:21:33 +0530

    08-9-routes-groups-and-layouts

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header style={{backgroundColor:"lightblue",padding:"1rem"}}>
+          <p>Header</p>
+        </header>
+        {children}
+        <footer style={{backgroundColor:"ghostwhite",padding:"1rem"}}>
+          <p>Footer</p>
+        </footer>
+        </body>
     </html>
   );
 }
