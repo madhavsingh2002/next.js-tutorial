1e10b383311c430e9db3f9b83e218878708ff039
madhav <madhav201singh@gmail.com>
2024-03-26 20:22:53 +0530

    16. Navigating Programmatically

---
+++ src/app/order-product/page.js
@@ -0,0 +1,19 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function OrderProduct(){
+    const router =useRouter();
+    const handleClick=()=>{
+        console.log("Placing Your Order")
+        router.push('/');
+    }
+    return( 
+        <>
+         <h1>Order Products</h1>
+         <button onClick={handleClick}>
+            Place Order
+         </button>
+        </>
+    )
+
+}
